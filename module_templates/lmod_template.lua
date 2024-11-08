04e43a11cfd4c84b3f94fc479f2e455d599d4ceb
Yucheng Zhang <Yucheng.Zhang@tufts.edu>
2024-11-07 21:06:48 -0500

    add function to generate lmod modules

---
+++ module_templates/lmod_template.lua
@@ -0,0 +1,30 @@
+help([==[
+
+Description
+===========
+nf-core is a global community effort to collect a curated set of open‑source analysis pipelines built using Nextflow. More information about nf-core/${PIPELINE} can be found at: https://nf-co.re/${PIPELINE}
+
+More information
+================
+ - Home page:     https://nf-co.re/${PIPELINE}
+]==])
+
+whatis("Pipeline: ${PIPELINE}")
+whatis("Version: ${VERSION}")
+whatis("Description: nf-core is a global community effort to collect a curated set of open‑source analysis pipelines built using Nextflow. More information about nf-core/${PIPELINE} can be found at: https://nf-co.re/${PIPELINE}")
+whatis("Home page:     https://nf-co.re/${PIPELINE}")
+
+-- Load the required modules
+depends_on("nextflow")
+
+-- Set Singularity Cache Dir
+setenv("NXF_SINGULARITY_CACHEDIR", "/cluster/tufts/biocontainers/nf-core/singularity-images")
+
+-- And assemble container command
+local pipeline_path = "/cluster/tufts/biocontainers/nf-core/pipelines/" .. myModuleName() .. "/" .. myModuleVersion() .. "/" .. "${VERSION_}"
+local pipeline_launch = "nextflow run " .. pipeline_path
+
+-- Programs to setup in the shell
+set_shell_function("${PIPELINE}", pipeline_launch .. " \"$@\"",
+                                pipeline_launch .. " $*")
+-- Additional commands or environment variables, if any
